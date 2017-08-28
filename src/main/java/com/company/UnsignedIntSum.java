30330868c977b6ca55087a889d7fb8313ffac829
Anastasiia Iurshina <anastasiia_iurshina@epam.com>
2017-08-28 23:38:21 +0300

    Add a paralleled version

--- src/main/java/com/company/UnsignedIntSum.java
+++ src/main/java/com/company/UnsignedIntSum.java
@@ -5,6 +5,9 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.channels.FileChannel;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.*;
 
 public class UnsignedIntSum {
 
@@ -25,6 +28,33 @@ public class UnsignedIntSum {
                 total += sum(mbb, curChunkSize);
             }
         } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return total;
+    }
+
+    public static long multiSumUnsignedInts(String path) {
+        long total = 0;
+
+        try (FileChannel fc = FileChannel.open(Paths.get(path))) {
+            final long size = fc.size();
+            final int chunks = (int) ((size + (CHUNK_SIZE - 1)) / CHUNK_SIZE);
+
+            ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() + 1);
+            Set<Future<Long>> futures = new HashSet<>();
+            for (int i = 0; i < chunks; i++) {
+                final long curChunkSize = Math.min(CHUNK_SIZE, size - CHUNK_SIZE * i);
+                final ByteBuffer mbb = fc.map(FileChannel.MapMode.READ_ONLY, CHUNK_SIZE * i, curChunkSize)
+                        .order(ByteOrder.LITTLE_ENDIAN);
+
+                futures.add(executor.submit(() -> sum(mbb, curChunkSize)));
+            }
+
+            for (Future<Long> f : futures) {
+                total += f.get();
+            }
+        } catch (IOException | InterruptedException | ExecutionException ex) {
             throw new RuntimeException(ex);
         }
 
